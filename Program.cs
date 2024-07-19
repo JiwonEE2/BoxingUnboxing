4ad2be3629999b18d92341815f67c3e10b08ba1e
jiwonchichi1 <jiwonchichi1@gmail.com>
2024-07-19 16:12:19 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BoxingUnboxing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 123;
+            object b = (object)a; // a에 담긴 값을 박싱해서 힙에 저장
+            int c = (int)b; // b에 담긴 값을 언박싱해서 스택에 저장
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            double x = 3.1414213;
+            object y = x;
+            double z = (double)y;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
